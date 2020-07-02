fe24cb3c10d59061faac4bcc7f51cc2fa1c906e5
Jun Chen <chenjun@caicloud.io>
2020-07-02 17:44:39 +0800

    feat: Init model registry and Support revserse proxy for harbor (#23)

---
+++ pkg/registry/server/harbor/handler.go
@@ -0,0 +1,58 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package harbor
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/astaxie/beego/context"
+	"github.com/spf13/viper"
+
+	"github.com/caicloud/temp-model-registry/pkg/registry/common"
+	"github.com/caicloud/temp-model-registry/pkg/registry/types"
+)
+
+func webhookAPI(c *context.Context) {
+	payload := types.Payload{}
+	if err := json.Unmarshal(c.Input.RequestBody, &payload); err != nil {
+		panic(err)
+	}
+	// TODO(simon): Create ModelJob to extract or convert
+
+	c.ResponseWriter.Write([]byte("OK"))
+}
+
+func projectAPIs(c *context.Context) {
+	ormbDomain := viper.GetString(common.ORMBDomainEnvKey)
+	ormbUserName := viper.GetString(common.ORMBUsernameEnvkey)
+	ormbPassword := viper.GetString(common.ORMBPasswordEnvKey)
+	auth := "basic " + base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%v:%v", ormbUserName, ormbPassword)))
+
+	proxy := httputil.NewSingleHostReverseProxy(c.Request.URL)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Header["Authorization"] = []string{auth}
+		req.Host = ormbDomain
+		req.URL.Host = ormbDomain
+		req.URL.Scheme = "http"
+		req.URL.Path = c.Request.URL.Path
+	}
+	proxy.ServeHTTP(c.ResponseWriter, c.Request)
+}
