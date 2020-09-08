4b2429465e426015322b3b47cb9c8a4212a79dad
simon-cj <chenjun@caicloud.io>
2020-09-08 17:19:54 +0800

    feat: Auto extract model when upload model

--- pkg/registry/modeljob/util.go
+++ pkg/registry/modeljob/util.go
@@ -1,8 +1,11 @@
 package modeljob
 
 import (
+	"fmt"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	modeljobsv1alpha1 "github.com/kleveross/klever-model-registry/pkg/apis/modeljob/v1alpha1"
 	"github.com/kleveross/klever-model-registry/pkg/common"
 	"github.com/kleveross/klever-model-registry/pkg/util"
 )
@@ -17,4 +20,28 @@ func ExchangeModelJobNameAndID(objectMeta *metav1.ObjectMeta) error {
 	objectMeta.Labels[common.ResourceNameLabelKey] = objectMeta.Name
 	objectMeta.Name = id
 	return nil
+}
+
+// GenerateExtractionModelJob will generate ModelJob by base information.
+func GenerateExtractionModelJob(domain, project, modelName, versionName, format string) *modeljobsv1alpha1.ModelJob {
+	modeljob := modeljobsv1alpha1.ModelJob{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "kleveross.io/v1alpha1",
+			Kind:       "ModelJob",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      util.RandomNameWithPrefix(fmt.Sprintf("modeljob-%v-%v-%v", project, modelName, versionName)),
+			Namespace: "default",
+		},
+		Spec: modeljobsv1alpha1.ModelJobSpec{
+			Model: fmt.Sprintf("%v/%v/%v:%v", domain, project, modelName, versionName),
+			ModelJobSource: modeljobsv1alpha1.ModelJobSource{
+				Extraction: &modeljobsv1alpha1.ExtractionSource{
+					Format: modeljobsv1alpha1.Format(format),
+				},
+			},
+		},
+	}
+
+	return &modeljob
 }
