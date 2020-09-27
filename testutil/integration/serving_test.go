2dcffa8affc83ed85eeb91f8b6299195d25f8a7e
simon-cj <chenjun@caicloud.io>
2020-09-27 14:16:12 +0800

    fix(serving): Update serving resources

--- testutil/integration/serving_test.go
+++ testutil/integration/serving_test.go
@@ -7,10 +7,12 @@ import (
 
 	httpexpect "github.com/gavv/httpexpect/v2"
 	. "github.com/onsi/ginkgo"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	// . "github.com/onsi/gomega"
 	seldonv1 "github.com/seldonio/seldon-core/operator/apis/machinelearning.seldon.io/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Model Registry", func() {
@@ -19,6 +21,16 @@ var _ = Describe("Model Registry", func() {
 	project := "library"
 	model := "tensorflow"
 	version := "test"
+
+	genTestResource := func() corev1.ResourceList {
+		resourcesList := make(corev1.ResourceList)
+		cpuQuantity, _ := resource.ParseQuantity("1")
+		resourcesList[corev1.ResourceCPU] = cpuQuantity
+		memQuantity, _ := resource.ParseQuantity("1Gi")
+		resourcesList[corev1.ResourceMemory] = memQuantity
+
+		return resourcesList
+	}
 
 	e := httpexpect.New(GinkgoT(), ModelRegistryHost)
 	Context("Servings", func() {
@@ -31,6 +43,23 @@ var _ = Describe("Model Registry", func() {
 				Spec: seldonv1.SeldonDeploymentSpec{
 					Predictors: []seldonv1.PredictorSpec{
 						{
+							ComponentSpecs: []*seldonv1.SeldonPodSpec{
+								{
+									Metadata: metav1.ObjectMeta{
+										Name: "test",
+									},
+									Spec: corev1.PodSpec{
+										Containers: []corev1.Container{
+											{
+												Resources: corev1.ResourceRequirements{
+													Limits:   genTestResource(),
+													Requests: genTestResource(),
+												},
+											},
+										},
+									},
+								},
+							},
 							Graph: seldonv1.PredictiveUnit{
 								Name:     "test",
 								ModelURI: fmt.Sprintf("%s/%s/%s:%s", ModelRegistryHost[7:], project, model, version),
