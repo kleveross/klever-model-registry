25358a529853157237332c3f32c01255507e1cad
Tianxin Dong <dongtianxin.dongtx@bytedance.com>
2020-11-23 15:59:17 +0800

    Feat(crd): init model job crd when run the controller (#178)
    * Feat(crd): init model job crd when run the controller
    
    * Fix(test): fix controller unit test
    
    * Fix(test): add crd apply in e2e test

--- cmd/modeljob-operator/app/server.go
+++ cmd/modeljob-operator/app/server.go
@@ -16,13 +16,25 @@ limitations under the License.
 package app
 
 import (
+	"bufio"
+	"bytes"
+	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/yaml"
 
 	"github.com/kleveross/klever-model-registry/cmd/modeljob-operator/app/options"
 	modeljobsv1alpha1 "github.com/kleveross/klever-model-registry/pkg/apis/modeljob/v1alpha1"
@@ -33,6 +45,13 @@ import (
 var (
 	scheme   = runtime.NewScheme()
 	setupLog = ctrl.Log.WithName("setup")
+)
+
+const (
+	// CustomResourceDefinitionKind is the kind of resource customresourcedefinition.
+	CustomResourceDefinitionKind = "CustomResourceDefinition"
+	// CustomResourceDefinitionPath is the path of crd yaml.
+	CustomResourceDefinitionPath = "crds"
 )
 
 func init() {
@@ -48,7 +67,8 @@ func Run(opt *options.ServerOption) error {
 		os.Exit(1)
 	}
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	config := ctrl.GetConfigOrDie()
+	mgr, err := ctrl.NewManager(config, ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: opt.MetricsAddr,
 		Port:               9443,
@@ -57,6 +77,11 @@ func Run(opt *options.ServerOption) error {
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
+		return err
+	}
+
+	if err := ensureCRD(config); err != nil {
+		setupLog.Error(err, "unable to start install modeljob crd")
 		return err
 	}
 
@@ -80,6 +105,112 @@ func Run(opt *options.ServerOption) error {
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		return err
+	}
+
+	return nil
+}
+
+func renderCRDs(crdpath string) ([]*apiextensionsv1beta1.CustomResourceDefinition, error) {
+	var (
+		info  os.FileInfo
+		files []os.FileInfo
+		dir   string
+	)
+
+	info, err := os.Stat(crdpath)
+	if err != nil {
+		return nil, err
+	}
+
+	if !info.IsDir() {
+		dir, files = filepath.Dir(crdpath), []os.FileInfo{info}
+	} else {
+		if files, err = ioutil.ReadDir(crdpath); err != nil {
+			return nil, err
+		}
+		dir = crdpath
+	}
+
+	return readCRDs(dir, files)
+}
+
+// readCRDs reads the CRDs from files and Unmarshals them into structs
+func readCRDs(basePath string, files []os.FileInfo) ([]*apiextensionsv1beta1.CustomResourceDefinition, error) {
+	crds := make([]*apiextensionsv1beta1.CustomResourceDefinition, 0, len(files))
+	crdExts := sets.NewString(".yaml", ".yml")
+	for _, file := range files {
+		// Only parse allowed file types
+		if !crdExts.Has(filepath.Ext(file.Name())) {
+			continue
+		}
+
+		// Unmarshal CRDs from file into structs
+		docs, err := readDocuments(filepath.Join(basePath, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		for _, doc := range docs {
+			crd := &apiextensionsv1beta1.CustomResourceDefinition{}
+			if err = yaml.Unmarshal(doc, crd); err != nil {
+				return nil, err
+			}
+
+			// Check that it is actually a CRD
+			if crd.Kind != CustomResourceDefinitionKind || crd.Spec.Names.Kind == "" || crd.Spec.Group == "" {
+				continue
+			}
+			crds = append(crds, crd)
+		}
+	}
+	return crds, nil
+}
+
+// readDocuments reads documents from file
+func readDocuments(fp string) ([][]byte, error) {
+	b, err := ioutil.ReadFile(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	// A file may contain multiple documents separated by `---`, eg:
+	//   a: 1
+	//   ---
+	//   a: 2
+	// will be converted to: ["a: 1", "a: 2"]
+	docs := make([][]byte, 0, 1)
+	reader := yamlutil.NewYAMLReader(bufio.NewReader(bytes.NewReader(b)))
+	for {
+		doc, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, err
+		}
+		docs = append(docs, doc)
+	}
+	return docs, nil
+}
+
+func ensureCRD(config *rest.Config) error {
+	client, err := clientset.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	crds, err := renderCRDs(CustomResourceDefinitionPath)
+	if err != nil {
+		return err
+	}
+	for _, crd := range crds {
+		_, err = client.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+		if err != nil {
+			if !errors.IsAlreadyExists(err) {
+				setupLog.Error(err, "unable to ensure crds")
+				return err
+			}
+		}
 	}
 
 	return nil
