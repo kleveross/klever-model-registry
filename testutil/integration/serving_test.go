e37d0a024250faee85ab8fe5811718bc9702ee78
Jinmei Shi <42922799+JinmeiShi@users.noreply.github.com>
2020-12-08 14:07:27 +0800

    test(E2ETest): Add e2e test cases for serving (#203)

--- testutil/integration/serving_test.go
+++ testutil/integration/serving_test.go
@@ -1,27 +1,31 @@
 package integration
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/caicloud/nirvana/log"
 	httpexpect "github.com/gavv/httpexpect/v2"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	// . "github.com/onsi/gomega"
+	"github.com/kleveross/klever-model-registry/pkg/util"
 	seldonv1 "github.com/seldonio/seldon-core/operator/apis/machinelearning.seldon.io/v1"
 )
 
 var _ = Describe("Model Registry", func() {
 	const timeout = time.Second * 5
 	const interval = time.Second * 1
+	var replica int32 = 1
 	project := "library"
-	model := "tensorflow"
-	version := "test"
+	model := "pmml"
+	version := "v1"
+	ns := "default"
+	servingName := util.RandomNameWithPrefix("serving-test")
 
 	genTestResource := func() corev1.ResourceList {
 		resourcesList := make(corev1.ResourceList)
@@ -35,7 +39,6 @@ var _ = Describe("Model Registry", func() {
 
 	e := httpexpect.New(GinkgoT(), ModelRegistryHost)
 	Context("Servings", func() {
-		name := "test"
 		It("Should create the Serving successfully", func() {
 			minReplicas := int32(1)
 			hpaSpec := seldonv1.SeldonHpaSpec{
@@ -44,7 +47,8 @@ var _ = Describe("Model Registry", func() {
 			}
 			seldonCoreDeploy := &seldonv1.SeldonDeployment{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: name,
+					Name: servingName,
+					Namespace: ns,
 				},
 				Spec: seldonv1.SeldonDeploymentSpec{
 					Predictors: []seldonv1.PredictorSpec{
@@ -52,7 +56,7 @@ var _ = Describe("Model Registry", func() {
 							ComponentSpecs: []*seldonv1.SeldonPodSpec{
 								{
 									Metadata: metav1.ObjectMeta{
-										Name: "test",
+										Name: servingName,
 									},
 									Spec: corev1.PodSpec{
 										Containers: []corev1.Container{
@@ -65,19 +69,17 @@ var _ = Describe("Model Registry", func() {
 										},
 									},
 									HpaSpec: &hpaSpec,
+									Replicas: &replica,
 								},
 							},
 							Graph: seldonv1.PredictiveUnit{
-								Name:     "test",
-								ModelURI: fmt.Sprintf("%s/%s/%s:%s", ModelRegistryHost[7:], project, model, version),
+								Name:     servingName,
+								ModelURI: fmt.Sprintf("harbor-harbor-core.harbor-system/%s/%s:%s", project, model, version),
+								ServiceAccountName: "default",
 								Parameters: []seldonv1.Parameter{
 									{
-										Name:  "cpu",
-										Value: "1",
-									},
-									{
-										Name:  "mem",
-										Value: "100Mi",
+										Name:  "format",
+										Value: "PMML",
 									},
 								},
 							},
@@ -85,24 +87,47 @@ var _ = Describe("Model Registry", func() {
 					},
 				},
 			}
-			e.POST("/api/v1alpha1/namespaces/{namespace}/servings", "default").WithJSON(seldonCoreDeploy).Expect().Status(http.StatusCreated)
-		})
-		It("Should get the Servings successfully", func() {
-			Eventually(func() int {
-				// TODO: better attersions
-				return int(e.GET("/api/v1alpha1/namespaces/{namespace}/servings/",
-					"default").Expect().Status(http.StatusOK).
-					JSON().Object().Value("items").Array().Length().Raw())
-			}, timeout, interval).Should(Equal(1))
+			e.POST("/api/v1alpha1/namespaces/{namespace}/servings", ns).WithJSON(seldonCoreDeploy).
+				Expect().Status(http.StatusCreated)
 		})
 
-		It("Should get the updated Serving with correct HPA", func() {
-			Eventually(func() bool {
-				_, ok := e.GET("/api/v1alpha1/namespaces/{namespace}/servings/{servingID}", "default", "test").
-					Expect().Status(http.StatusOK).JSON().Object().Value("spec").Object().Value("predictors").
-					Array().Element(0).Object().Value("componentSpecs").Array().Element(0).Object().Raw()["hpaSpec"]
-				return ok
-			}, timeout, interval).Should(BeTrue())
+		It("The list should contains the Serving", func() {
+			Eventually(func() []byte {
+				str, err := json.Marshal(e.GET("/api/v1alpha1/namespaces/{namespace}/servings/", ns).
+					Expect().Status(http.StatusOK).
+					JSON().Object().Raw())
+				if err != nil{
+					log.Error(err)
+				}
+				return str
+			}, timeout, interval).Should(ContainSubstring(servingName))
 		})
+
+		It("Should get the Serving successfully", func() {
+			body := e.GET("/api/v1alpha1/namespaces/{namespace}/servings/{servingID}", ns, servingName).
+				Expect().Status(http.StatusOK).Body().Raw()
+			sdep := seldonv1.SeldonDeployment{}
+			json.Unmarshal([]byte(body),&sdep)
+			Expect(sdep.Spec.Predictors[0].Graph.ModelURI).Should(ContainSubstring(project+"/"+model+":"+version))
+			Expect(sdep.ObjectMeta.Name).Should(Equal(servingName))
+			Expect(sdep.Spec.Predictors[0].ComponentSpecs[0].Spec.Containers[0].Resources.Requests.Cpu().String()).Should(Equal("1"))
+			Expect(sdep.Spec.Predictors[0].ComponentSpecs[0].Spec.Containers[0].Resources.Requests.Memory().String()).Should(Equal("1Gi"))
+			Expect(sdep.Spec.Predictors[0].ComponentSpecs[0].Spec.Containers[0].Resources.Limits.Cpu().String()).Should(Equal("1"))
+			Expect(sdep.Spec.Predictors[0].ComponentSpecs[0].Spec.Containers[0].Resources.Limits.Memory().String()).Should(Equal("1Gi"))
+		})
+
+		It("should delete the serving successfully", func() {
+			e.DELETE("/api/v1alpha1/namespaces/{namespace}/servings/{servingID}", ns, servingName).
+				Expect().Status(http.StatusNoContent)
+		})
+
+		It("Shouldn't get the deleted serving", func() {
+			Eventually(func() int {
+				return e.GET("/api/v1alpha1/namespaces/{namespace}/servings/{servingID}", ns, servingName).
+					Expect().Raw().StatusCode
+			}, timeout, interval).Should(Equal(http.StatusNotFound))
+
+		})
+
 	})
 })
