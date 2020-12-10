573f949ffdf69328fa0b4b16ce6353c99e57c38e
simon-cj <chenjun@caicloud.io>
2020-12-10 14:23:22 +0800

    test(ut): dep latest ormb

--- testutil/integration/model_test.go
+++ testutil/integration/model_test.go
@@ -15,10 +15,7 @@ import (
 	"github.com/kleveross/ormb/pkg/oras"
 	"github.com/kleveross/ormb/pkg/ormb"
 
-	"path/filepath"
-	"github.com/spf13/viper"
 	"os"
-	"github.com/sirupsen/logrus"
 
 	"fmt"
 	"net/http"
@@ -28,7 +25,6 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 )
 
 var _ = Describe("Model Registry", func() {
@@ -54,15 +50,14 @@ var _ = Describe("Model Registry", func() {
 	}
 
 	var modelConvert = []struct {
-		model   string
-		tomodel string
-		version string
-		format  modeljobsv1alpha1.Format
+		model    string
+		tomodel  string
+		version  string
+		format   modeljobsv1alpha1.Format
 		toformat modeljobsv1alpha1.Format
-
 	}{
-		{"h5", "tensorflow","v1", modeljobsv1alpha1.FormatH5, modeljobsv1alpha1.FormatSavedModel},
-		{"caffe", "netdef","v1", modeljobsv1alpha1.FormatCaffeModel, modeljobsv1alpha1.FormatNetDef},
+		{"h5", "tensorflow", "v1", modeljobsv1alpha1.FormatH5, modeljobsv1alpha1.FormatSavedModel},
+		{"caffe", "netdef", "v1", modeljobsv1alpha1.FormatCaffeModel, modeljobsv1alpha1.FormatNetDef},
 		{"mxnetparams", "onnx", "v1", modeljobsv1alpha1.FormatMXNETParams, modeljobsv1alpha1.FormatONNX},
 	}
 
@@ -73,23 +68,19 @@ var _ = Describe("Model Registry", func() {
 		//check the model status
 		It("Should list the model successfully", func() {
 			//Upload the model by ormb
-			var tool ormb.Interface
 			plainHTTPOpt := true
 			var insecureOpt bool
 			project := "library"
-			rootPath, err := filepath.Abs(viper.GetString("./models"))
-			Expect(err).Should(BeNil())
-			logrus.WithFields(logrus.Fields{
-				"root-path": rootPath,
-			}).Debugln("Create the ormb client with the given root path")
 
-			tool, err = ormb.New(
-				oras.ClientOptRootPath(rootPath),
+			tool, err := ormb.New(
 				oras.ClientOptWriter(os.Stdout),
 				oras.ClientOptPlainHTTP(plainHTTPOpt),
 				oras.ClientOptInsecure(insecureOpt),
-				)
-			for _, tt := range modelExtract{
+			)
+			tool.Login(ModelRegistryHost[7:], "admin", "Harbor12345", true)
+			Expect(err).Should(BeNil())
+
+			for _, tt := range modelExtract {
 				dirtag := fmt.Sprintf("%s/%s/%s:%s", ModelRegistryHost[7:], project, tt.model, tt.version)
 				err = tool.Save("./models/"+tt.model, dirtag)
 				Expect(err).Should(BeNil())
@@ -109,7 +100,7 @@ var _ = Describe("Model Registry", func() {
 		Context("Convert ModelJobs", func() {
 			It("Should create the ModelJobs successfully", func() {
 				for _, tt := range modelConvert {
-					name := tt.model+"2"+tt.tomodel
+					name := tt.model + "2" + tt.tomodel
 					desiredtag := fmt.Sprintf("%s/%s/%s:%s", ModelRegistryHost[7:], project, tt.tomodel, "new")
 					job := modeljobsv1alpha1.ModelJob{
 						ObjectMeta: metav1.ObjectMeta{
@@ -117,14 +108,14 @@ var _ = Describe("Model Registry", func() {
 						},
 						Spec: modeljobsv1alpha1.ModelJobSpec{
 							// Use ModelRegistryHost/<project>/<model>:<version>.
-							Model: fmt.Sprintf("%s/%s/%s:%s", ModelRegistryHost[7:], project, tt.model, tt.version),
+							Model:      fmt.Sprintf("%s/%s/%s:%s", ModelRegistryHost[7:], project, tt.model, tt.version),
 							DesiredTag: &desiredtag,
 							ModelJobSource: modeljobsv1alpha1.ModelJobSource{
 								Conversion: &modeljobsv1alpha1.ConversionSource{
 									MMdnn: &modeljobsv1alpha1.MMdnnSpec{
 										modeljobsv1alpha1.ConversionBaseSpec{
 											From: tt.format,
-											To: tt.toformat,
+											To:   tt.toformat,
 										},
 									},
 								},
@@ -137,7 +128,7 @@ var _ = Describe("Model Registry", func() {
 			//check the modeljob is created
 			It("Should list the ModelJobs successfully", func() {
 				for _, tt := range modelConvert {
-					name := tt.model+"2"+tt.tomodel
+					name := tt.model + "2" + tt.tomodel
 					modelJobs := e.GET("/api/v1alpha1/namespaces/{namespace}/modeljobs/",
 						"default").Expect().Status(http.StatusOK).JSON().Object().Value("items").Array()
 					found := false
@@ -164,7 +155,7 @@ var _ = Describe("Model Registry", func() {
 			// get modeljob event
 			It("Should get the ModelJob events successfully", func() {
 				for _, tt := range modelConvert {
-					name := tt.model+"2"+tt.tomodel
+					name := tt.model + "2" + tt.tomodel
 					e.GET("/api/v1alpha1/namespaces/{namespace}/modeljobs/{modeljobID}/events",
 						"default", name).Expect().Status(http.StatusOK)
 				}
@@ -195,7 +186,7 @@ var _ = Describe("Model Registry", func() {
 			})
 			//check the modeljob is created
 			It("Should list the ModelJobs successfully", func() {
-				for _, tt := range modelExtract{
+				for _, tt := range modelExtract {
 					name := tt.model + "extract"
 					modelJobs := e.GET("/api/v1alpha1/namespaces/{namespace}/modeljobs/",
 						"default").Expect().Status(http.StatusOK).JSON().Object().Value("items").Array()
@@ -214,10 +205,11 @@ var _ = Describe("Model Registry", func() {
 			})
 
 			It("Should get the ModelJob successfully", func() {
-				for _, tt := range modelExtract{
+				for _, tt := range modelExtract {
 					name := tt.model + "extract"
 					e.GET("/api/v1alpha1/namespaces/{namespace}/modeljobs/{modeljobID}",
-						"default", name).Expect().Status(http.StatusOK)}
+						"default", name).Expect().Status(http.StatusOK)
+				}
 
 			})
 
